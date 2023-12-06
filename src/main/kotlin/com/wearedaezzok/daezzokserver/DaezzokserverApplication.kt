63867e3768cb16836e647625e3c20b4f26dba930
unanchoi <unanc2@gmail.com>
2023-12-06 21:32:00 +0900

    initial commit

---
+++ src/main/kotlin/com/wearedaezzok/daezzokserver/DaezzokserverApplication.kt
@@ -0,0 +1,11 @@
+package com.wearedaezzok.daezzokserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DaezzokserverApplication
+
+fun main(args: Array<String>) {
+    runApplication<DaezzokserverApplication>(*args)
+}
---
+++ src/test/kotlin/com/wearedaezzok/daezzokserver/DaezzokserverApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wearedaezzok.daezzokserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DaezzokserverApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
